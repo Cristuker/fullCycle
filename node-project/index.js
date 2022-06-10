558afcbe34be9f9f9ad8c66ebabd20681f0a97f4
Cristuker <cristian123105@gmail.com>
2022-06-10 09:03:46 -0300

    criando serviço com node e mysql

--- node-project/index.js
+++ node-project/index.js
@@ -1,10 +1,23 @@
 // @ts-check
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES('Cristian');`;
+connection.query(sql);
+connection.end();
 
 app.get('/', (_req, res) => {
-    res.send('<h1>Full Cycle</h1>')
+    res.send('<h1>Full Cycle</h1>');
 });
 
 app.listen(port, () => {
